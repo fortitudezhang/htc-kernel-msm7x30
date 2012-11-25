8e8557690cb8c6d5a85c27c4c0ae7d5d24f9693d
e334 <e334cn@gmail.com>
2012-11-24 16:07:54 -0800

    First pass of board files, kernel now boots

--- arch/arm/mach-msm/board-lexikon-wifi.c
+++ arch/arm/mach-msm/board-lexikon-wifi.c
@@ -9,7 +9,11 @@
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <linux/skbuff.h>
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+#include <linux/wlan_plat.h>
+#else
 #include <linux/wifi_tiwlan.h>
+#endif
 
 #include "board-lexikon.h"
 
@@ -77,7 +81,11 @@ static struct resource lexikon_wifi_resources[] = {
 		.name		= "bcm4329_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(LEXIKON_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(LEXIKON_GPIO_WIFI_IRQ),
+#ifdef CONFIG_BCM4329_PURE_ANDROID
+		.flags		= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
+#else
 		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
+#endif
 	},
 };
 
@@ -86,7 +94,10 @@ static struct wifi_platform_data lexikon_wifi_control = {
 	.set_reset      = lexikon_wifi_reset,
 	.set_carddetect = lexikon_wifi_set_carddetect,
 	.mem_prealloc   = lexikon_wifi_mem_prealloc,
+#ifndef CONFIG_BCM4329_PURE_ANDROID
 	.dot11n_enable  = 1,
+	.cscan_enable   = 1,
+#endif
 };
 
 static struct platform_device lexikon_wifi_device = {
